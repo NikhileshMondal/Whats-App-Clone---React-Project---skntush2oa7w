02bdb2a0213a9358bed6d048026c6a6bdfaf5792
NikhileshMondal <109812970+NikhileshMondal@users.noreply.github.com>
2023-02-18 20:17:02 +0530

    Add files via upload

---
+++ src/mockData.jsx
@@ -0,0 +1,78 @@
+export const contactList = [
+  {
+    id: 1,
+    name: "Nikhilesh",
+    profilePic:
+      "https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+    lastText: "Hey Man",
+    lastTextTime: "12:58 PM",
+  },
+  {
+    id: 2,
+    name: "Rohan",
+    profilePic:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    lastText: `Hi`,
+    lastTextTime: "12:45 PM",
+  },
+  {
+    id: 3,
+    name: "Ankana",
+    profilePic:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    lastText: "Where is are you?",
+    lastTextTime: "12:30 PM",
+  },
+  {
+    id: 4,
+    name: "Titly",
+    profilePic:
+      "https://images.unsplash.com/photo-1518305860742-0d7119d4567f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8U1hudXR0RVRlYUF8fGVufDB8fHx8&auto=format&fit=crop&w=420&q=60",
+    lastText: "I am Travelling",
+    lastTextTime: "12:00 PM",
+  },
+];
+export const messagesList = [
+  {
+    id: 1,
+    messageType: "TEXT",
+    text: "Hey Man 😁",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 2,
+    messageType: "TEXT",
+    text: "Hey, What's up?",
+    senderID: 1,
+    addedOn: "12:01 PM",
+  },
+  {
+    id: 3,
+    messageType: "TEXT",
+    text: "All Good, What about you?",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 4,
+    messageType: "TEXT",
+    text: "I'm good as well",
+    senderID: 1,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 5,
+    messageType: "TEXT",
+    text: "Great 😁",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 5,
+    messageType: "TEXT",
+    text: "how was work going on?",
+    senderID: 1,
+    addedOn: "12:00 PM",
+  },
+];
